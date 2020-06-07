e03c6f2450290fc1cc926cf4059110850433ec96
Chirag Khandhar <ckhandhar@hawk.iit.edu>
2020-06-07 10:02:34 -0500

    Splash Screen Added

---
+++ app/src/main/java/ml/chiragkhandhar/retagged/SplashScreen.java
@@ -0,0 +1,26 @@
+package ml.chiragkhandhar.retagged;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
